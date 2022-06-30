db01a9676da35abdd3db7f4cbfe30d079e770cf5
Pechuan Jorge, Ximo {GRMW~South San Francisco} <pechuan-jorge.joaquin@gene.com>
2022-06-30 12:19:02 -0700

    Fixed main file

--- src/BID.jl
+++
@@ -1,6 +0,0 @@
-module BID
-include("crns.jl")
-include("math.jl")
-include("analytical.jl")
-include("plots.jl")
-end
--- src/EvolutionaryModels.jl
+++ src/EvolutionaryModels.jl
@@ -1,4 +1,9 @@
 module EvolutionaryModels
 include("Blundell_Types.jl")
 include("Blundell_Functions.jl")
+include("Blundell_Functions.jl")
+include("crns.jl")
+include("math.jl")
+include("analytical.jl")
+include("plots.jl")
 end
