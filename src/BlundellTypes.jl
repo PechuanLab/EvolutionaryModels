696eb5ebac4b1e06673cc0353b3650478e8ba681
Ximo Pechuan i Jorge <lionzord@gmail.com>
2023-11-08 09:11:49 -0800

    Add files via upload

---
+++ src/BlundellTypes.jl
@@ -0,0 +1,21 @@
+export Mutation
+export Clone
+export Population
+
+abstract type Lineages end
+
+struct Mutation <: Lineages
+    ID::Int
+    Fitness::Float64
+end
+
+mutable struct Clone <: Lineages
+    Mutations::Array{Mutation}
+    N::Int
+end
+
+mutable struct Population <: Lineages
+    clones::Array{Clone}
+end
+
+
